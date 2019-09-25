7fd468c976f6585a4ea74c3aa9a902feb1f74749
Dhruv Sharma <dhruv_sharma001@icloud.com>
2019-09-25 18:08:09 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React from 'react';
-
-import HomePage from './homepage.component';
+import { Switch, Route } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
+function App() {
+  return (
+    <div className='App'>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
