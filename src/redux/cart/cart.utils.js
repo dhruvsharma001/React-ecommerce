8705cbf951e4d9f938d98b2d2ef4d7d8d8543518
Dhruv Sharma <dhruv_sharma001@icloud.com>
2019-09-30 19:40:36 +0530

    cart item dropdown added

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,16 @@
+//Utility functions allow us to keep out files clean and organize func that we may need in multiple files in one location
+
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
